23ee8821db81e08034ef1f609cbd6489cd06bb26
Ishan Dandekar <ishandandekar2002@gmail.com>
2025-04-19 17:53:02 +0530

    fsu

--- agent/speech/stt.py
+++ agent/speech/stt.py
@@ -1,89 +1,14 @@
-# import torch
-# from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
-# from datasets import load_dataset
-
-
-# device = "cuda:0" if torch.cuda.is_available() else "cpu"
-# torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-
-# model_id = "distil-whisper/distil-large-v3"
-
-# model = AutoModelForSpeechSeq2Seq.from_pretrained(
-#     model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
-# )
-# model.to(device)
-
-# processor = AutoProcessor.from_pretrained(model_id)
-
-# pipe = pipeline(
-#     "automatic-speech-recognition",
-#     model=model,
-#     tokenizer=processor.tokenizer,
-#     feature_extractor=processor.feature_extractor,
-#     max_new_tokens=128,
-#     torch_dtype=torch_dtype,
-#     device=device,
-# )
-
-# # dataset = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
-# # sample = dataset[0]["audio"]
-# audio_path = "sample.mp4"  
-# from pathlib import Path
-
-# if not Path(audio_path).exists():
-#     raise FileNotFoundError(f"Audio file not found at {audio_path}")
-
-# result = pipe(audio_path)
-# print(result["text"])
-
-# import torch
-# from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
-# from datasets import load_dataset
-
-
-# device = "cuda:0" if torch.cuda.is_available() else "cpu"
-# torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-
-# model_id = "distil-whisper/distil-large-v3"
-
-# model = AutoModelForSpeechSeq2Seq.from_pretrained(
-#     model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True,
-# )
-# model.to(device)
-
-# processor = AutoProcessor.from_pretrained(model_id)
-
-# pipe = pipeline(
-#     "automatic-speech-recognition",
-#     model=model,
-#     tokenizer=processor.tokenizer,
-#     feature_extractor=processor.feature_extractor,
-#     max_new_tokens=128,
-#     torch_dtype=torch_dtype,
-#     device=device,
-    
-# )
-
-# audio_path = "sample.mp4"  
-# from pathlib import Path
-
-# if not Path(audio_path).exists():
-#     raise FileNotFoundError(f"Audio file not found at {audio_path}")
-
-# result = pipe(audio_path, return_timestamps=True)
-# print(result)
-import os 
+import os
 from dotenv import load_dotenv
 from openai import OpenAI
+
 load_dotenv()
-API_KEY=os.getenv('OPENAI_API_KEY')
+API_KEY = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI(api_key=API_KEY)
-audio_file= open(r"D:\PersonalProjects\ExpeBot\agent\speech\sample.mp4", "rb")
+audio_file = open(r"D:\PersonalProjects\ExpeBot\agent\speech\sample.mp4", "rb")
 
-transcription = client.audio.transcriptions.create(
-    model="whisper-1", 
-    file=audio_file
-)
+transcription = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
 
 print(transcription.text)
+
--- agent/speech/stt_agent.py
+++ agent/speech/stt_agent.py
@@ -1,36 +1,10 @@
-# from agents import Agent
-# from agents import Runner
-# from pydantic import BaseModel
-
-
-# PROMPT = """
-# You are a Financial Data Extractor. Your task is to extract the following information based on the transaction.
-
-# Make sure you follow this schema:
-# 1. Description: (Short description if the transaction)
-# 2. Amount: (Money spent)
-# 3. Category: (If provided else Misc.)
-
-# There may be more than one transaction in a given text, You **must** identify and extract all of them. 
-#  """
-
-# agent = Agent(
-#     name="Financial Data Extractor",
-#     instructions=PROMPT,
-# )
-
-
-# async def main():
-#     result = await Runner.run(agent, "I Spent 5000 on alcohol today at 9 am ")
-#     print(result.final_output)
-
-
 from agents import Agent, Runner
 from pydantic import BaseModel
 import typing as T
 import asyncio
 from dotenv import load_dotenv
-load_dotenv(r'D:\PersonalProjects\ExpeBot\.env')
+
+load_dotenv(r"D:\PersonalProjects\ExpeBot\.env")
 
 
 async def run_agent(input_text):
@@ -47,15 +21,14 @@ async def run_agent(input_text):
         As output, you must produce a
         structured output containing the relevant schema.
         """,
-        output_type=Transaction)
-
-    result = await Runner.run(
-        starting_agent=finance_agent,
-        input=input_text
+        output_type=Transaction,
     )
+
+    result = await Runner.run(starting_agent=finance_agent, input=input_text)
     return result
 
 
-if __name__ =='__main__':
-    result=asyncio.run(run_agent('I Spent 5000 on alcohol today at 9 am'))
+if __name__ == "__main__":
+    result = asyncio.run(run_agent("I Spent 5000 on alcohol today at 9 am"))
     print(result)
+
